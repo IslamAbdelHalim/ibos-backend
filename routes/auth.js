98e0749aaa62139a01fa3f1303bbaa86a25e87b1
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-08-14 12:43:06 +0300

    add secure to token

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post('/register', async (req, res) => {
     const userId = newUser._id;
     //Generate Token
     const token = jwt.sign({id: newUser._id, email: newUser.email}, process.env.SECRET_KEY || "secret", {expiresIn: "1d"});
-    res.cookie('token', token, {httpOnly: true});
+    res.cookie('token', token, {httpOnly: true, secure: true});
     res.status(201).json({
       token,
       userId,
