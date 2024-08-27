f98472bd593284843636870e4b95e152f02b1a27
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-08-27 17:51:16 +0300

    remove not important code

--- routes/settings.js
+++ routes/settings.js
@@ -18,6 +18,7 @@ router.get('/', verifyToken, async (req, res) => {
     if (!user) return res.status(404).json({ status: "error", message: 'User not found' });
     res.status(200).json({ status: "success", message: "User found", user });
   } catch (err) {
+    console.log(err)
     res.status(500).json({ status: "error", message: err.message });
   }
 });
